9cbaf4a40b36bd167f3a0b930fe66bc53364713d
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-10-03 13:45:19 +0900

    nav

--- uniflee/app/src/main/java/com/android/myapplication/MainActivity.kt
+++ uniflee/app/src/main/java/com/android/myapplication/MainActivity.kt
@@ -5,16 +5,46 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.myapplication.databinding.ActivityMainBinding
+import com.android.myapplication.membership.MembershipMainFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        setBottomNavigationView()
+
+        // 앱 초기 실행 시 홈화면으로 설정
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_home
+        }
+    }
+
+    fun setBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, HomeFragment()).commit()
+                    true
+                }
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, SearchFragment()).commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, MembershipMainFragment()).commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, SettingsFragment()).commit()
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
