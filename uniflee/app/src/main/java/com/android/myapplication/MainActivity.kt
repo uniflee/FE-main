0b60020e10f78209cb7dbf1a50b15c76ca3524a1
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-10-04 21:57:30 +0900

    [UI] main, tip 미완성

--- uniflee/app/src/main/java/com/android/myapplication/MainActivity.kt
+++ uniflee/app/src/main/java/com/android/myapplication/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.android.myapplication.databinding.ActivityMainBinding
 import com.android.myapplication.discharge.DischargeMainFragment
 import com.android.myapplication.membership.MembershipMainFragment
@@ -18,33 +20,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        setBottomNavigationView()
-
-        // 앱 초기 실행 시 홈화면으로 설정
+        // 처음 실행할 때 기본 Fragment 설정
         if (savedInstanceState == null) {
-            binding.bottomNavigationView.selectedItemId = R.id.fragment_discharge
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, DischargeMainFragment())
+                .addToBackStack(null)
+                .commit()
         }
-    }
-
-    fun setBottomNavigationView() {
 
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.fragment_discharge -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.main_container, DischargeMainFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, DischargeMainFragment())
+                        .addToBackStack(null)
+                        .commit()
                     true
                 }
                 R.id.fragment_store -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.main_container, StoreMainFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, StoreMainFragment())
+                        .addToBackStack(null)
+                        .commit()
                     true
                 }
                 R.id.fragment_membership -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.main_container, MembershipMainFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MembershipMainFragment())
+                        .addToBackStack(null)
+                        .commit()
                     true
                 }
                 R.id.fragment_my -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.main_container, MyMainFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MyMainFragment())
+                        .addToBackStack(null)
+                        .commit()
                     true
                 }
                 else -> false
--- uniflee/app/src/main/java/com/android/myapplication/membership/MembershipMainFragment.kt
+++ uniflee/app/src/main/java/com/android/myapplication/membership/MembershipMainFragment.kt
@@ -6,55 +6,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.myapplication.R
+import com.android.myapplication.databinding.FragmentMembershipMainBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MembershipMainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MembershipMainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentMembershipMainBinding?=null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_membership_main, container, false)
+    ): View {
+        _binding = FragmentMembershipMainBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        binding.goGradeBtn.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MembershipGradeFragment())
+                .addToBackStack(null) // 백스택에 추가해서 뒤로 가기 가능
+                .commitAllowingStateLoss()
+        }
+        binding.goGradeInfo.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MembershipGradeInfoFragment())
+                .addToBackStack(null) // 백스택에 추가해서 뒤로 가기 가능
+                .commitAllowingStateLoss()
+        }
+        binding.goGradeTips.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MembershipTipsFragment())
+                .addToBackStack(null) // 백스택에 추가해서 뒤로 가기 가능
+                .commitAllowingStateLoss()
+        }
+        binding.goGradeBenefit.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MembershipBenefitFragment())
+                .addToBackStack(null) // 백스택에 추가해서 뒤로 가기 가능
+                .commitAllowingStateLoss()
+        }
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MembershipMainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MembershipMainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onResume() {
+        super.onResume()
+        binding.root.requestLayout()
     }
+
 }
