be0ab52d9f81f0b84de68fd96137fd2e05b28f9a
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-10-30 17:19:27 +0900

    [UI] new reward 완성

--- uniflee/app/src/main/java/com/android/myapplication/discharge/DischargeNewRewardActivity.kt
+++ uniflee/app/src/main/java/com/android/myapplication/discharge/DischargeNewRewardActivity.kt
@@ -13,12 +13,35 @@ class DischargeNewRewardActivity : AppCompatActivity() {
     private val binding: ActivityDischargeNewRewardBinding by lazy {
         ActivityDischargeNewRewardBinding.inflate(layoutInflater)
     }
+    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
         binding.backBtn.setOnClickListener {
             onBackPressed()
+        }
+        val textNumber = binding.textNumber
+        val buttonMinus = binding.buttonMinus
+        val buttonPlus = binding.buttonPlus
+
+        // 초기 숫자 설정
+        textNumber.text = number.toString()
+
+        // 마이너스 버튼 클릭 시
+        buttonMinus.setOnClickListener {
+            if (number>0){
+                number--
+            }
+            textNumber.text = number.toString()
+        }
+
+        // 플러스 버튼 클릭 시
+        buttonPlus.setOnClickListener {
+            if (number<5){
+                number++
+            }
+            textNumber.text = number.toString()
         }
     }
 }
